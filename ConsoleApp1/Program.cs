a4af7e227c05eadb7b764f471427252e039a2255
thelomik <akardos11@gmail.com>
2022-12-11 20:13:12 +0100

    test

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+
+var url = "https://localhost:7292/WeatherForecast";
+
+var request = WebRequest.Create(url);
+request.Method = "GET";
+
+using var webResponse = request.GetResponse();
+using var webStream = webResponse.GetResponseStream();
+
+using var reader = new StreamReader(webStream);
+var data = reader.ReadToEnd();
+
+Console.WriteLine(data);
+
