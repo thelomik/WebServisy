181b8204b5af3eeccdee0441c4db39e989d8a372
thelomik <akardos11@gmail.com>
2022-12-11 20:10:13 +0100

    Dodaj pliki projektów.

---
+++ WebServisy/Controllers/WeatherForecastController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace WebServisy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+      
+        [HttpPost]
+        public string FunkcjaSkrotu(string text)
+        {
+            return text.Do10Znakow();
+        }
+        
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            ;
+        }
+
+
+
+        
+
+    }
+}
---
+++ WebServisy/RozszerzeniaString.cs
@@ -0,0 +1,13 @@
+﻿namespace WebServisy
+{
+    public static class RozszerzeniaString
+    {
+        public static string Do10Znakow(this string text)
+        {
+            if (text.Length >= 10)
+                return text.Substring(0, 10);
+            else
+                return text.PadRight(10, 'X');
+        }
+    }
+}
